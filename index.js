43c3a30060d0b13fe0d567cf9f3246f789aaf6d9
simha <bhupendrasimha@gmail.com>
2021-03-07 20:37:21 +0530

    Drag and Drop

---
+++ index.js
@@ -0,0 +1,51 @@
+// Using IIFE instead of Window onload event listiner
+(function () {
+  for (
+    var items = document.querySelectorAll('[data-draggable="item"]'),
+      len = items.length,
+      i = 0;
+    i < len;
+    i++ ) {
+    items[i].setAttribute("draggable", "true");
+   }
+
+  var item = null;
+
+  document.addEventListener(
+    "dragstart",
+    function (e) {
+      item = e.target;
+      e.dataTransfer.setData("text", "");
+    },
+    false
+  );
+
+  document.addEventListener(
+    "dragover",
+    function (e) {
+      if (item) {
+        e.preventDefault();
+      }
+    },
+    false
+  );
+
+  document.addEventListener(
+    "drop",
+    function (e) {
+      if (e.target.getAttribute("data-draggable") == "target") {
+        e.target.appendChild(item);
+        e.preventDefault();
+      }
+    },
+    false
+  );
+
+  document.addEventListener(
+    "dragend",
+    function (e) {
+      item = null;
+    },
+    false
+  );
+})();
